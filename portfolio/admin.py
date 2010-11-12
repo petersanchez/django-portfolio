a506fcc73efc13c44f89595e647038feb70ac610
Peter Sanchez <petersanchez@gmail.com>
2010-11-11 21:08:26 -0800

    Updated admin.py for prepopulated_fields

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -5,10 +5,19 @@ from portfolio.models import *
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'url', 'is_active',)
     list_editable = ('is_active',)
+    prepopulated_fields = {'slug': ('name',),}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',),}
+
+
+class SkillAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',),}
 
 
 admin.site.register(Project, ProjectAdmin)
-admin.site.register(Skill)
-admin.site.register(Category)
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(ProjectFile)
 admin.site.register(ProjectImage)
